c71d3434c7fa728d436899a534920212f2654bda
gautamTech03 <gautamdakua877@gmail.com>
2025-07-16 19:58:51 +0530

    fix: image tag removed from home

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,7 +1,6 @@
 export default function Home(){
     return(
     <div>
-        <img src="https://th.bing.com/th/id/OIP.6M_tdY7Fbu5RvJ_A3B4xMwHaHa?w=184&h=184&c=7&r=0&o=7&pid=1.7&rm=3"/>
         <p>Professional Gautam Dakua</p>
     </div>
 )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Navbar from "@/components/navbar";
-import Image from "next/image";
 
 export default function Home() {
   return (
    
     <>
     <Navbar/>
-    <img src="https://th.bing.com/th/id/OIP.6M_tdY7Fbu5RvJ_A3B4xMwHaHa?w=184&h=184&c=7&r=0&o=7&pid=1.7&rm=3"/>
     </>
   );
 }
